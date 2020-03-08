ac992a5ee1706879a4169288b140dc14df203c26
slash <theophile.roos@gmail.com>
2020-03-08 21:34:17 +0100

    ADD: Connection between ui signals and Player instance methods

--- src/Player.py
+++ src/Player.py
@@ -8,8 +8,9 @@ class Player():
     def __init__(self, library):
         pm.init()
         self.library = library
-        self.index = 1
+        self.index = 2
         self.sound = None
+        self.paused = True
         self.initialise()
         
     
@@ -18,32 +19,45 @@ class Player():
 
         
     def next_track(self):
+        print("next")
         self.index +=1
         self._load()
-        self.play()
+        self._play()
 
-    def previous_song(self):
+    def previous_track(self):
+        print("previous")
         self.index += -1
         self._load()
-        self.play()
+        self._play()
 
 
     def _load(self):
         track = self.library.get_track_number(self.index)
         self.sound = pm.Sound(track.get_abs_path())
         
+    def launch(self):
+        self._play()
+        self.paused = False
 
 
 
     def _get_state(self):
         return pm.get_busy() # NOTE: return bool
 
-    def play(self):
+    def _play(self):
         if not self._get_state():
             self.sound.play()
+            
         else:
             self.stop()
             self.sound.play()
+            
+    def pause_resume(self):
+        if self.paused:
+            self.unpause()
+        else:
+            self.pause()
+
 
     def boucle(self):
         if not self._get_state():
@@ -51,13 +65,22 @@ class Player():
 
     def pause(self):
         pm.pause()
+        self.paused = True
 
     def unpause(self):
         pm.unpause()
-    
+        self.paused = False
     
     
     def stop(self):
         pm.stop()
 
     
+
+if __name__ == "__main__":
+    import pathlib
+    path = pathlib.Path("/home/slash/Documents/Programmation/littlePythonPlayer/music") # DEBUG: not for prod
+    library = Library.Library(path)
+    p = Player(library)
+    p.launch()
+
--- src/main.py
+++ src/main.py
@@ -2,24 +2,45 @@ import Library
 import Player
 import pathlib
 import time
-class App():
+from PySide2 import QtWidgets
+from ui import window
+
+class App(QtWidgets.QMainWindow, window.Ui_Window):
     def __init__(self):
+        super().__init__()
+        self.ui = window.Ui_Window()
+        self.ui.setupUi(self)
         path = pathlib.Path("/home/slash/Documents/Programmation/littlePythonPlayer/music") # DEBUG: not for prod
         self.library = Library.Library(path)
         self.player = Player.Player(self.library)
-        self.ui = None
+        self.setup_connections()
+        self.player.launch()
+
+        #self.ui = None
+
+    def setup_connections(self):
+        self.ui.btn_playpause.clicked.connect(self.player.pause_resume)
+        self.ui.btn_next.clicked.connect(self.player.next_track)
+        self.ui.btn_previous.clicked.connect(self.player.previous_track)
 
     
 
 if __name__ == "__main__":
+    qt_app = QtWidgets.QApplication()
     app = App()
-    app.player.play()
+    app.show()
+    qt_app.exec_() 
+
+
+
+    """app.player.play()
     while 1:
         try:
             time.sleep(0.001)
             app.player.boucle()
 
         except KeyboardInterrupt:
+            app.player.stop()
             print("end")
-            break
+            break"""
              
